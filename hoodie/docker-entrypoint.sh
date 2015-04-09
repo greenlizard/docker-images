6de0b61c685f9386bb274cb543346f3e0d5e4fc7
Gabriel Mancini <gabriel.mancini@gmail.com>
2015-04-09 17:45:37 -0300

    fix permissions

--- hoodie/docker-entrypoint.sh
+++ hoodie/docker-entrypoint.sh
@@ -6,6 +6,12 @@ until [ "`curl -X PUT $COUCH_URL/_config/admins/$HOODIE_ADMIN_USER -d '"'$HOODIE
     sleep 0.1;
 done;
 
+if [ "$1" = 'hoodie' ]; then
+  # we need to set the permissions here because docker mounts volumes as root
+  chown -R xroot:xroot /src
+  HOME=/src exec gosu xroot "$@"
+fi
+
 #Set Admin on couchdb
 rm -f /src/data/hoodie.pid
 HOME=/src exec "$@"
